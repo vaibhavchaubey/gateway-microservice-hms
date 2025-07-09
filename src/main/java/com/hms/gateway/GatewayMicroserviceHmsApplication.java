a302de0fb3c49ca6ce0e1af3678e087b4c58620a
Vaibhav <vaibhavchaubey786@gmail.com>
2025-07-09 17:31:10 +0530

    first commit

---
+++ src/main/java/com/hms/gateway/GatewayMicroserviceHmsApplication.java
@@ -0,0 +1,13 @@
+package com.hms.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayMicroserviceHmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayMicroserviceHmsApplication.class, args);
+	}
+
+}
